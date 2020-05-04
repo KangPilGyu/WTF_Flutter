e9d114e8d9e4e44bcf4215bd5ecfafec6d3efdda
KangPilGyu <eiffeltop01@gmail.com>
2020-05-04 21:12:49 +0900

    base architecture

--- lib/core/models/user.dart
+++ lib/core/models/user.dart
@@ -1,7 +1,12 @@
+import 'package:json_annotation/json_annotation.dart';
+part 'user.g.dart';
+
+@JsonSerializable()
 class User {
   int id;
   String name;
   String username;
+
   User({this.id, this.name, this.username});
 
   User.initial()
@@ -9,17 +14,7 @@ class User {
         name = '',
         username = '';
 
-  User.fromJson(Map<String, dynamic> json) {
-    id = json['id'];
-    name = json['name'];
-    username = json['username'];
-  }
+  factory User.fromJson(Map<String, dynamic> json) => _$UserFromJson(json);
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['id'] = this.id;
-    data['name'] = this.name;
-    data['username'] = this.username;
-    return data;
-  }
+  Map<String, dynamic> toJson() => _$UserToJson(this);
 }
